208e6ff7544f9815132e7d3ec5dc6717ac931184
omar-faruk02 <74614887+omar-faruk02@users.noreply.github.com>
2021-02-28 17:19:14 +0600

    1st

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import Countryinfo from './Components/Countryinfo/Countryinfo';
+
+
 
 function App() {
+  const [contiries,setcountries]=useState([])
+  useEffect(( )=>{
+    fetch('https://restcountries.eu/rest/v2/all')
+    .then(res=>res.json())
+    .then(data=>{
+      setcountries(data)
+    }
+      )
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h3>Number of country:{contiries.length} </h3>
+    
+      {
+        contiries.map(country=><Countryinfo country={country}></Countryinfo>
+       
+      )
+      }
+    
     </div>
   );
 }
---
+++ src/Components/Countryinfo/Countryinfo.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './countryinfo.css';
+
+const Countryinfo = (props) => {
+    const countryinfo=props.country
+   const {name,flag,capital,region,population,area,currencies,languages}=countryinfo
+   for (var i=0;i<currencies.length;i++)
+   {
+     var currency=currencies[i].name
+      
+   }
+   for (var i=0;i<languages.length;i++)
+   {
+     var language=languages[i].name
+      
+   }
+    return (
+        <div className="info">
+          <div className="country">
+          <h3>Name: {name}</h3>
+            <img src={flag} alt=""/>
+            <p>Caital: {capital}</p>
+            <p>region: {region}</p>
+            <p>population: {population}</p>
+            <p>area: {area}</p>
+            <p>currencies: {currency}</p>
+            <p>languages: {language}</p>
+          </div>
+        </div>
+    );
+};
+
+export default Countryinfo;
